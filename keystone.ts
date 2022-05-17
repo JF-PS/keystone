1dc2f69a50f6c2baccd0d7526e0648755f770ab7
JF-PS <jfps.dev21@gmail.com>
2022-05-17 12:40:44 +0200

    test

--- keystone.ts
+++ keystone.ts
@@ -5,8 +5,8 @@ import { withAuth, session } from "./auth";
 export default withAuth(
   config({
     db: {
-      provider: "sqlite",
-      url: "file:./keystone.db",
+      provider: "postgresql",
+      url: "postgres://postgres:alexisadrien@dots-database-1.c8s1fkaqyt9l.eu-west-3.rds.amazonaws.com:5432/dots-schemas--staging",
     },
     ui: {
       isAccessAllowed: (context) => !!context.session?.data,
